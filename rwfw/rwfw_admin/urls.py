ebfb9f102c3ba1d102cca6919dc479b699eafe6b
nfvguru@gmail.com <nfvguru@gmail.com>
2020-05-17 22:18:06 +0530

    Initial template based main project

--- rwfw/rwfw_admin/urls.py
+++ rwfw/rwfw_admin/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('rwfw/', include('rwfw_main.urls')),
+    path('', include('rwfw_main.urls')),
     path('admin/', admin.site.urls),
 ]
