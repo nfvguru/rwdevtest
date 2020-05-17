af70a164a3a135e66150298b774f8ad7c727874a
nfvguru@gmail.com <nfvguru@gmail.com>
2020-05-17 22:48:00 +0530

    Display data from database1

--- rwfw/rwfw_main/views.py
+++ rwfw/rwfw_main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import rwfw_item_table
 
 # Create your views here.
 def index(request):
     #return HttpResponse("RWFW Initial.")
-    return render(request,'index.html')
+    db1_obj = rwfw_item_table.objects
+    return render(request,'index.html','items':db1_obj)
