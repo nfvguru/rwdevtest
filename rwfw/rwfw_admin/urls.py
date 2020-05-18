6ad86cb1841f92102e458a77f008d8d46639e2ae
nfvguru@gmail.com <nfvguru@gmail.com>
2020-05-19 00:08:48 +0530

    Media content Display

--- rwfw/rwfw_admin/urls.py
+++ rwfw/rwfw_admin/urls.py
@@ -23,3 +23,6 @@ urlpatterns = [
     path('', include('rwfw_main.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
