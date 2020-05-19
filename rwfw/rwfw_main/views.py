237ddd1910fc1f01f9f8a3dfabb923083ef05cae
nfvguru@gmail.com <nfvguru@gmail.com>
2020-05-19 12:28:01 +0530

    add links

--- rwfw/rwfw_main/views.py
+++ rwfw/rwfw_main/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import rwfw_item_table
 
 # Create your views here.
 def index(request):
     #return HttpResponse("RWFW Initial.")
-    db1_obj = rwfw_item_table.objects
+    db1_obj = rwfw_item_table.objects.all()
     return render(request,'index.html',{'items':db1_obj})
 
 def activities(request, item_id):
-    print(item_id)
-    return render(request, 'index.html')
+    #print(item_id)
+    item_link=get_object_or_404(rwfw_item_table, pk=item_id)
+    return render(request, 'items.html', {'item_link':item_link})
