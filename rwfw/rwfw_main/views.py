0dade640370e3d0fbcfebdc2b2f4aebfc95cfac9
nfvguru@gmail.com <nfvguru@gmail.com>
2020-05-19 13:16:04 +0530

    reverting links and debugging commits

--- rwfw/rwfw_main/views.py
+++ rwfw/rwfw_main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponse
 from .models import rwfw_item_table
 
@@ -6,12 +6,8 @@ from .models import rwfw_item_table
 def index(request):
     #return HttpResponse("RWFW Initial.")
     db1_obj = rwfw_item_table.objects.all()
-    for titem in db1_obj:
-       print(titem.item_icon.url)
-
     return render(request,'index.html',{'items':db1_obj})
 
 def activities(request, item_id):
-    #print(item_id)
-    item_link=get_object_or_404(rwfw_item_table, pk=item_id)
-    return render(request, 'items.html', {'item_link':item_link})
+    print(item_id)
+    return render(request, 'index.html')
