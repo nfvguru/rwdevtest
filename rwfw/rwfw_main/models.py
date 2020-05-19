4f2887023936dfe9c5d78f13a0b2f35ff84afbf2
nfvguru@gmail.com <nfvguru@gmail.com>
2020-05-19 15:45:37 +0530

    updating DB with new column

--- rwfw/rwfw_main/models.py
+++ rwfw/rwfw_main/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 class rwfw_item_table(models.Model):
     item_icon = models.ImageField(upload_to='images/')
+    item_head = models.CharField(max_length=30, default='Manager')
     item_desc = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.item_desc
+        return self.item_head
         
