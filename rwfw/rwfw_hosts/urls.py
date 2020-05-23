53912795e5300b4251ee37472fe43702bdcc2108
nfvguru@gmail.com <nfvguru@gmail.com>
2020-05-24 02:24:12 +0530

    host manager2 local bootstrap

--- rwfw/rwfw_hosts/urls.py
+++ rwfw/rwfw_hosts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import re_path
+from django.urls import re_path, path
 from .import views
 
 
@@ -6,6 +6,6 @@ from .import views
 urlpatterns = [
     # re_path(r'/Host Manager', views.hostmanager,name="hostmanager"),
     # path('<int:item_id>', views.activities,name="rwfwops"),
-    re_path(r'^HostAdd', views.hostadd, name="hostadd"),
+    path('ListHost/<typename>', views.hostlist, name="hostlist"),
     re_path(r'^Host', views.hostmanager, name="hostmanager"),
 ]
--- rwfw/rwfw_hosts/views.py
+++ rwfw/rwfw_hosts/views.py
@@ -1,11 +1,27 @@
+import os
+import subprocess
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import rwfw_host_type
+from .models import rwfw_host_table
 
 # Create your views here.
 def hostmanager(request):
     # print(mhost)
-    return HttpResponse("Host Manager Da")
+    db1_obj = rwfw_host_type.objects.all()
+    return render(request,'hosts.html',{'items':db1_obj})
+    # return HttpResponse("Host Manager Da")
 
 
-def hostadd(request):
-    return HttpResponse("Add Host")
+def hostlist(request,typename):
+    # print(typename)
+    db1_obj = rwfw_host_type.objects.filter(type_name=typename)
+    db2_obj = rwfw_host_table.objects.filter(host_type=db1_obj[0].id)
+    # print(db2_obj[0].host_name)
+    command = 'ping -n 1 ' + db2_obj[0].host_ip
+     # Can even be 'python manage.py somecommand'
+    child = subprocess.Popen(command, shell=True)
+    # returncode = child.poll()
+    print(child.returncode)
+    # return HttpResponse("Host List")
+    return render(request,'listhost.html',{'tpname':typename,'tpurl':db1_obj[0].type_img, 'hosttable':db2_obj})
