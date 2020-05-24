7e41cc73e1167d9e680d77f901e59751d28d414b
nfvguru@gmail.com <nfvguru@gmail.com>
2020-05-24 21:31:15 +0530

    Order based on new param

--- rwfw/rwfw_main/views.py
+++ rwfw/rwfw_main/views.py
@@ -6,7 +6,7 @@ from .models import rwfw_item_table
 # Create your views here.
 def index(request):
     #return HttpResponse("RWFW Initial.")
-    db1_obj = rwfw_item_table.objects.all().order_by('id')
+    db1_obj = rwfw_item_table.objects.all().order_by('item_order')
     visit_count = request.session.get('num_visit',0) + 1
     request.session['num_visit'] = visit_count
     return render(request,'index.html',{'items':db1_obj,'visit_count':visit_count})
