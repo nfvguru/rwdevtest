936b28bec6824d3e4720619c9c3ac972983ac13f
nfvguru@gmail.com <nfvguru@gmail.com>
2020-05-24 16:49:40 +0530

    List host with bootstrap

--- rwfw/rwfw_hosts/views.py
+++ rwfw/rwfw_hosts/views.py
@@ -1,5 +1,5 @@
 import os
-import subprocess
+import subprocess as sp
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import rwfw_host_type
@@ -15,13 +15,46 @@ def hostmanager(request):
 
 def hostlist(request,typename):
     # print(typename)
+    public_cloud = 0
+    context = {'tpname':typename}
+    if typename == 'Azure':
+        public_cloud = 1
+    elif typename == 'AWS':
+        public_cloud = 2
     db1_obj = rwfw_host_type.objects.filter(type_name=typename)
-    db2_obj = rwfw_host_table.objects.filter(host_type=db1_obj[0].id)
-    # print(db2_obj[0].host_name)
-    command = 'ping -n 1 ' + db2_obj[0].host_ip
-     # Can even be 'python manage.py somecommand'
-    child = subprocess.Popen(command, shell=True)
-    # returncode = child.poll()
-    print(child.returncode)
-    # return HttpResponse("Host List")
-    return render(request,'listhost.html',{'tpname':typename,'tpurl':db1_obj[0].type_img, 'hosttable':db2_obj})
+    db2_obj = rwfw_host_table.objects.none();
+    try:
+        # print(db1_obj[0].id)
+        # print(db1_obj[0].type_name)
+        db2_obj = rwfw_host_table.objects.filter(host_type=db1_obj[0].id)
+        context['tpurl']=db1_obj[0].type_img
+    except:
+        # print("Query Error")
+        context['tpurl'] = ''
+    context['hosttable'] = db2_obj
+    context['reachable'] = 0
+    host_ip = ""
+    if public_cloud == 1:
+        host_ip = 'portal.azure.com'
+        context['host_ip'] = host_ip
+        context['reachable'] = 1
+    elif public_cloud == 2:
+        host_ip = 'console.aws.amazon.com'
+        context['host_ip'] = host_ip
+        context['reachable'] = 1
+    elif db2_obj.exists():
+        host_ip = db2_obj[0].host_ip
+        # print(db2_obj[0].host_name)
+        print(os.name)
+        ping_opt = '-c 1 '
+        if os.name == 'nt':
+            ping_opt = '-n 1 '
+        command = 'ping ' + ping_opt + host_ip
+        # print(command)
+        # Can even be 'python manage.py somecommand'
+        child = sp.Popen(command, shell=True,stdout=sp.PIPE)
+        streamdata = child.communicate()[0]
+        rc = child.returncode
+        if rc == 0:
+            context['reachable'] = 1
+    return render(request,'listhost.html',context)
