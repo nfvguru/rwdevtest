9e3bddde2cdf1772ce21adfb496ca9d278d5cd94
nfvguru@gmail.com <nfvguru@gmail.com>
2020-05-25 22:10:25 +0530

    Fixing Error

--- rwfw/rwfw_admin/celery.py
+++ rwfw/rwfw_admin/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_parroter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rwfw_admin.settings')
 
-celery_app = Celery('image_parroter')
+celery_app = Celery('rwfw_admin')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
