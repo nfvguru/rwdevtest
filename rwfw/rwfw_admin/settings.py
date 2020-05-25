431df3dba2959e69af321844de204bfe9f05cd51
nfvguru@gmail.com <nfvguru@gmail.com>
2020-05-25 22:06:20 +0530

    Adding celery to the project

--- rwfw/rwfw_admin/settings.py
+++ rwfw/rwfw_admin/settings.py
@@ -131,3 +131,10 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'')
+
+# celery
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
