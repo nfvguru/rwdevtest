debcc84d00c85ae80933beba0c076022653295d5
nfvguru@gmail.com <nfvguru@gmail.com>
2020-05-31 00:06:33 +0530

    remote login

--- rwfw/rwfw_local/utils/rwfw_ssh_utils.py
+++ rwfw/rwfw_local/utils/rwfw_ssh_utils.py
@@ -5,6 +5,13 @@ from pexpect import pxssh
 def test_ssh():
     s = pxssh.pxssh()
     s.login('10.75.20.75', 'lava', 'lava')
+
+    s.sendline('uptime')   # run a command
+    s.prompt()             # match the prompt
+    print(s.before)        # print everything before the prompt.
+    s.sendline('ls -l')
+    s.prompt()
+    print(s.before)
     s.logout()
 
-test_ssh
+test_ssh()
